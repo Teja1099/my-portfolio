286a0421f1fa1daf0716af8cf7358583f74bc08b
saitejapulluri <pullurisaiteja10@gmail.com>
2025-05-01 16:38:22 -0500

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.png" alt="" fill className="object-contain" />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Crafting Real-World Software with Scalable Architecture
+          </h1>
+          {/* DESC */}
+          <p className="md:text-xl">
+            I design and develop software that’s not just functional—but scalable, maintainable, and user-focused. From architecting microservices to refining frontend interactions, my work is rooted in thoughtful engineering and a drive for excellence.
+          </p>
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              Explore My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Let’s Connect
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
